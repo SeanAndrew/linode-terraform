32222a8c6c4b79633a0ace27cc5807fb7cc16de3
Sean Chapman <cce4him@gmail.com>
2021-08-27 14:24:56 -0400

    first commit

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "linode" {
+  token = "linode-api-key"
+}
+
+resource "linode_instance" "nextcloud-instance" {
+    label = "nextcloud"
+    image = "linode/debian11"
+    region = "us-east"
+    type = "g6-standard-2"
+    authorized_keys = ["ssh-rsa AAAA...."]
+    root_pass = "terr4form-test"
+    stackscript_id = 888417
+    swap_size = 256
+    private_ip = true
+}
